b870d834bc3d82a109498d4768dac2378fc1ef84
unknown <giorgiapridonidze08@gmail.com>
2025-11-21 12:08:39 +0400

    feat : added text and buttons in Main.jsx

--- frontend/src/container/Main.jsx
+++ frontend/src/container/Main.jsx
@@ -1,7 +1,15 @@
 const Main = () => {
     return(
         <div className="main-container">
-            
+            <div className="title">
+                <h1>Driver Tickets</h1>
+            </div>
+
+            <div className="buttons row row-cols-2">
+                <button className="btn btn-primary">Start Exam</button>
+                <button className="btn btn-primary">Saved Tickets</button>
+            </div>
+
         </div>
     )
 }
