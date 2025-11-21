1dac4fb7dc1a02156631d1cfaffa642421e2dbc4
unknown <giorgiapridonidze08@gmail.com>
2025-11-21 12:30:52 +0400

    feat : created api to sends data folder from backend to frontend

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const dataRoute = express.Router()
 
+const tickets = require('../data/tikets.json')
+const audio = require('../data/audio')
+const imgs  = require('../data/tickets')
 
-dataRoute.get('/', (req,res) => {
-    return res.status(200).json('data route')
+dataRoute.get('/', async (req,res) => {
+    try {
+
+        return res.status(200).json({tickets : tickets , audio : audio , imgs : imgs})
+
+    }catch(err){
+        return res.status(500).json('internal error')
+    }
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ const app = express()
 
 const PORT = 8080 ; //move to env
 
-
-app.get('/' , (req,res) => {
-    res.send('/')
-})
-
 app.use('/data' , dataRoute);
 
 app.listen(PORT , () => {console.log(`http://localhost:${PORT}`)} )
