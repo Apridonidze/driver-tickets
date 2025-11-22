658d956725b95c1cc816a6058baab7838c93a5f1
unknown <giorgiapridonidze08@gmail.com>
2025-11-22 13:00:59 +0400

    feat : created token generation in cookies.js , that generates random cookie for user and sends to frontend

--- backend/routes/cookies.js
+++ backend/routes/cookies.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const cookiesProvider = express.Router()
 
-cookiesProvider.get('/' , (req,res) => {
-    console.log('cookies provider')
-})
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require("uuid");
 
+cookiesProvider.get('/', (req, res) => {
 
-module.exports = cookiesProvider
+    try{
+            
+        const payload = {userId : uuidv4()}
+
+        const token = jwt.sign(payload , 'secret_key' , {expiresIn : '30d'})
+
+        return res.status(200).json({token : token})
+
+    }catch(err){
+        return res.status(500).json('internal error')
+    }
+    
+});
+
+module.exports = cookiesProvider;
