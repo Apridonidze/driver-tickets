e22b5a2c61178d8d1198686fb8a4eb88e065ad29
unknown <giorgiapridonidze08@gmail.com>
2025-11-22 16:59:26 +0400

    feat : pushed answered ticket in database

--- backend/routes/tickets.js
+++ backend/routes/tickets.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const verifyToken = require('../config/verifyToken')
 const ticketRoute = express.Router()
+const db = require('../db/db')
 
 ticketRoute.get('/answered-tickets' , (req,res) => {
     
@@ -11,8 +12,14 @@ ticketRoute.post('/post-answered-tickets' , verifyToken, async (req,res) => {
     
     try{
         
-        let data = {userId : req.user , answeredTicket : req.body.answeredTicket.map(ticket => ticket)}
+        let data = {userId : req.user , answeredTicket : req.body.answeredTicketLast}
 
+        let ticketQueries =  db.query('insert into answeredTickets (user_id, ticketId, answerId, correctId) values ( ? , ? , ? , ? )' , [data.userId ,data.answeredTicket.ticketId , data.answeredTicket.answerId , data.answeredTicket.correctId])
+        let ticketResponse = await Promise.all(ticketQueries)
+
+
+        return res.status(200).json(ticketResponse[0][0])
+        
 
     }catch(err){
         return res.status(500).json('interval error')
