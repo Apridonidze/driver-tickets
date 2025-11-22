bf6db8ff075e7600c19835d8c1284ca0e3d45595
unknown <giorgiapridonidze08@gmail.com>
2025-11-22 15:31:46 +0400

    feat : created database with users table in it (database/schema.sql)

--- backend/routes/saved.js
+++ backend/routes/saved.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const verifyToken = require('../config/verifyToken')
 const savedRoute = express.Router()
 
-savedRoute.get('/saved-tickets', (req,res) => {
+savedRoute.get('/saved-tickets', verifyToken ,(req,res) => {
     
 })
 
-savedRoute.post('/post-saved-tickets', (req,res) => {
+savedRoute.post('/post-saved-tickets', verifyToken ,(req,res) => {
     console.log(req.body)
 })
 
---
+++ database/schema.sql
@@ -0,0 +1,5 @@
+create database driver_tickets_db
+use driver_tickets_db
+
+/**creates users table **/
+create table users (user_id int unique auto_increment primary key)
