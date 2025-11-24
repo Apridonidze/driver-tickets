9229d8c81bc12b495d7227fbd9dfbef54096d6fc
unknown <giorgiapridonidze08@gmail.com>
2025-11-24 13:40:16 +0400

    feat : created delete-saved-tickets api to unsave ticket after clicking on save button second time

--- backend/routes/saved.js
+++ backend/routes/saved.js
@@ -41,6 +41,21 @@ savedRoute.post('/post-saved-tickets', verifyToken , async (req,res) => {
 savedRoute.delete('/delete-saved-tickets' , verifyToken , async (req,res) => {
     
     const data = {ticketId : req.body.data , userId : req.user}
+
+    try{
+
+        const [checkSaved] = await db.query('select * from savedtickets where user_id = ? and ticket_id = ?' [data.userId , data.ticketId])
+
+        if(checkSaved.length === 0) return res.status(400).json('ticket not saved')
+
+        await db.query('delete from savedtickets where user_id = ? , ticket_id = ?' , [data.userId , data.ticketId])
+
+        return res.status(200).json('unsaved')
+
+    }catch(err){
+        return res.status(500).json('internal error')
+    }
+
 })
 
 
