f35cde78c265a91b814c8ece63af95507e24bb83
unknown <giorgiapridonidze08@gmail.com>
2025-11-25 16:24:21 +0400

    feat : created token based route acccess, if user does not have token then it is send to main page where frontend sends request to backend to genrerate token and return to user , if user tries to navigate to other pages then they are directed to main page untill they have token

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,23 +1,24 @@
 import { useCookies } from 'react-cookie'
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 import Main from '../frontend/src/container/Main'
 import Favorite from '../frontend/src/container/Favorite'
 import Exam from '../frontend/src/container/Exam'
 
+import '../frontend/src/index.css'
 
 
 
 const App = () => {
 
     const [ cookies ] = useCookies(['token']) ;//cookies
-    const hasToken = cookies.token && cookies.token !== "undefined"; //checks if user has cookies
+    const hasToken = cookies.token && cookies.token != undefined ; //checks if user has cookies
 
     const router = createBrowserRouter([
     
         {path : '/'  , element : <Main />},
-        {path : '/exam'  , element : <Exam/>}, 
-        {path : '/saved-tickets'  , element : <Favorite/>},
+        {path : '/exam'  , element : hasToken ? <Exam/> : <Navigate to='/' />},
+        {path : '/saved-tickets'  , element : hasToken ? <Favorite/> : <Navigate to='/' />},
         {path : '*'  , element : <Main/>},
 
     ])
