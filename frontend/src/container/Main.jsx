6bfcc44fad795d021cb8df2a699eaf4348d1b3bd
unknown <giorgiapridonidze08@gmail.com>
2025-11-25 16:06:28 +0400

    style : add main color to whole project , made Main.jsx responsive

--- frontend/src/container/Main.jsx
+++ frontend/src/container/Main.jsx
@@ -29,23 +29,23 @@ const Main = () => {
 
     return(
         <div className="main-container container d-flex flex-column justify-content-between" style={{minHeight : '100vh'}}>
-            <div className="title">
-                <h1>Driver Tickets</h1>
-                <h4>Welcome to Driver Tickets, where you can practice for upcoming theoretical driving exam</h4>
+            <div className="title text-center align-items center p-5">
+                <h1 className="pb-4">Driver Tickets</h1>
+                <h4>Welcome to Driver Tickets, where you can practice for upcoming theoretical driving exam.</h4>
             </div>
 
-            <div className="buttons row row-cols-2">
-                <Link to='/exam' className="btn btn-primary">Start Exam</Link>
-                <Link to='/saved-tickets' className="btn btn-primary">Saved Tickets</Link>
+            <div className="buttons row row-cols-1 gap-3 d-flex flex-column align-items-center ">
+                <Link to='/exam' className="btn btn-primary fs-3 w-50">Start Exam</Link>
+                <Link to='/saved-tickets' className="btn btn-primary fs-3 w-50">Saved Tickets</Link>
             </div>
 
             <div className="footer row border-top border-3 p-2">
-                <div className="col border-end p-2">
+                <div className="col col-12 col-sm-4 border-end p-2">
                     <h5>© 2025 Driver Tickets. All rights reserved.</h5>
                     <h6>This platform is for educational purposes and is not an official government exam.</h6>
                 </div>
 
-                <div className="col border-end p-2">
+                <div className="col col-12 col-sm-4 border-end p-2">
                     <h5>Feedback & Support</h5>
                     <h6>Have suggestions or found an issue?</h6>
                     <h6>Contact : <i class="fa-regular fa-envelope"></i> <a href="mailto:giorgiapridonidze08@gmail.com">giorgiapridonidze08@gmail.com</a></h6>
