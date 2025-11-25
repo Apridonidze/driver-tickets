ea15e02f4c55f91c30c83be411404496daaf57d6
unknown <giorgiapridonidze08@gmail.com>
2025-11-25 15:58:53 +0400

    style : added linkable icon to main apge in Header.jsx

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -4,10 +4,9 @@ const Header = () => {
     return(
         <div className="header-container container-fluid d-flex justify-content-between align-items-center my-1 border-bottom border-3">
             <div className="header-start">
-                <h3><i class="fa-solid fa-car"></i> Driver-Tickets</h3>
+                <Link to='/' className="link link-dark fs-3 link-underline-opacity-0"><i class="fa-solid fa-car"></i> Driver-Tickets</Link>
             </div>
             <div className="header-end d-flex gap-2 fs-6">
-                <Link to='/' className="link link-dark link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Home</Link>
                 <Link to='/exam' className="link link-dark link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Exam</Link>
                 <Link to='/saved-tickets' className="link link-dark link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Saved</Link>
             </div>
--- frontend/src/container/Main.jsx
+++ frontend/src/container/Main.jsx
@@ -28,14 +28,33 @@ const Main = () => {
 
 
     return(
-        <div className="main-container">
+        <div className="main-container container d-flex flex-column justify-content-between" style={{minHeight : '100vh'}}>
             <div className="title">
                 <h1>Driver Tickets</h1>
+                <h4>Welcome to Driver Tickets, where you can practice for upcoming theoretical driving exam</h4>
             </div>
 
             <div className="buttons row row-cols-2">
                 <Link to='/exam' className="btn btn-primary">Start Exam</Link>
                 <Link to='/saved-tickets' className="btn btn-primary">Saved Tickets</Link>
+            </div>
+
+            <div className="footer row border-top border-3 p-2">
+                <div className="col border-end p-2">
+                    <h5>© 2025 Driver Tickets. All rights reserved.</h5>
+                    <h6>This platform is for educational purposes and is not an official government exam.</h6>
+                </div>
+
+                <div className="col border-end p-2">
+                    <h5>Feedback & Support</h5>
+                    <h6>Have suggestions or found an issue?</h6>
+                    <h6>Contact : <i class="fa-regular fa-envelope"></i> <a href="mailto:giorgiapridonidze08@gmail.com">giorgiapridonidze08@gmail.com</a></h6>
+                </div>
+
+                <div className="col p-2">
+                    <h5>Created By Giorgi Aphridonidze.</h5>
+                </div>
+
             </div>
 
         </div>
