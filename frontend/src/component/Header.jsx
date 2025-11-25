89c03ff3c0e7a36c10f776bc0defae932885fc10
unknown <giorgiapridonidze08@gmail.com>
2025-11-25 16:02:45 +0400

    style : styled main.jsx folder

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
     return(
         <div className="header-container container-fluid d-flex justify-content-between align-items-center my-1 border-bottom border-3">
             <div className="header-start">
-                <Link to='/' className="link link-dark fs-3 link-underline-opacity-0"><i class="fa-solid fa-car"></i> Driver-Tickets</Link>
+                <Link to='/' className="link link-light fs-3 link-underline-opacity-0"><i class="fa-solid fa-car"></i> Driver-Tickets</Link>
             </div>
             <div className="header-end d-flex gap-2 fs-6">
-                <Link to='/exam' className="link link-dark link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Exam</Link>
-                <Link to='/saved-tickets' className="link link-dark link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Saved</Link>
+                <Link to='/exam' className="link link-light link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Exam</Link>
+                <Link to='/saved-tickets' className="link link-light link-underline-opacity-0 link-underline-opacity-75-hover link-offset-3">Saved</Link>
             </div>
         </div>
     )
