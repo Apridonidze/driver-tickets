c079b23fa219f334a4db7617e31ef367017ce782
unknown <giorgiapridonidze08@gmail.com>
2025-11-25 16:40:31 +0400

    cleaned up and documenatted App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,13 +1,11 @@
-import { useCookies } from 'react-cookie'
-import { Navigate, RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { Navigate, RouterProvider, createBrowserRouter} from 'react-router-dom';//importing react libraries
 
-import Main from '../frontend/src/container/Main'
-import Favorite from '../frontend/src/container/Favorite'
-import Exam from '../frontend/src/container/Exam'
+import Main from '../frontend/src/container/Main';
+import Favorite from '../frontend/src/container/Favorite'; 
+import Exam from '../frontend/src/container/Exam';//importing reawct component
 
-import '../frontend/src/index.css'
-
-
+import '../frontend/src/index.css'; //importing css file
 
 const App = () => {
 
@@ -17,18 +15,16 @@ const App = () => {
     const router = createBrowserRouter([
     
         {path : '/'  , element : <Main />},
-        {path : '/exam'  , element : hasToken ? <Exam/> : <Navigate to='/' />},
-        {path : '/saved-tickets'  , element : hasToken ? <Favorite/> : <Navigate to='/' />},
+        {path : '/exam'  , element : hasToken ? <Exam/> : <Navigate to='/' />}, //navigates user to main page if they do not have token
+        {path : '/saved-tickets'  , element : hasToken ? <Favorite/> : <Navigate to='/' />},//navigates user to main page if they do not have token
         {path : '*'  , element : <Main/>},
 
-    ])
+    ]); //routes array
     
 
     return (
         <RouterProvider  router={router}/>
-    )
-}
+    ); //returning routes for app
+};
 
-
-
-export default App
+export default App; //exporting component
