35ce423b514310fe83187ea7fd8a152a097e0e58
unknown <giorgiapridonidze08@gmail.com>
2025-11-26 13:09:27 +0400

    doc : documentated database/schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,15 @@
+
+/* created database */
 create database driver_tickets_db
 use driver_tickets_db
 
-/**creates users table **/
+
+
+/** creates users table **/
 create table users (user_id varchar(155) unique primary key)
 
 
-/**creates answers table **/
+/** creates answers table **/
 create table answeredTickets (user_id varchar(155), ticketId int , answerId int , correctId int )
 
 /** creates saved tickets table **/
