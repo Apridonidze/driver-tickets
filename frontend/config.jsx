32a611a731364c5d03dd78c254d57e612e1d466a
unknown <giorgiapridonidze08@gmail.com>
2025-11-26 21:55:45 +0400

    installed run build

--- frontend/config.jsx
+++ frontend/config.jsx
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080'
-    : process.env.REACT_APP_BACKEND_URL;
+  ? 'http://localhost:8080'
+  : process.env.REACT_APP_BACKEND_URL;
