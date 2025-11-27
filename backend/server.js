5cd35f998c1ae8b11781f420bf1b07dd81cf78fb
unknown <giorgiapridonidze08@gmail.com>
2025-11-27 17:20:57 +0400

    ipdate

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,38 @@
-const express = require('express'); //importing express
-const app = express(); //using express as app
-
+const express = require('express'); // importing express
+const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser'); //importiung cors and body-parser for CORS issues
+require('dotenv').config(); // importing dotenv
 
-require('dotenv').config(); //importing dotenv
+const path = require('path'); // <-- add this
 
-
+// Routes
 const cookiesProvider = require('./routes/cookies');
 const savedRoute = require('./routes/saved');
 const ticketRoute = require('./routes/tickets');
-const dataRoute = require('./routes/data');//importing routes
+const dataRoute = require('./routes/data');
 
-const CorsOptions = require('./config/CorsOptions');//importing cors options
+// CORS options
+const CorsOptions = require('./config/CorsOptions');
+
+// Middleware
+app.use(cors(CorsOptions));
+app.use(express.json()); // parses JSON requests
+
+// API Routes
+app.use('/data', dataRoute);
+app.use('/cookies', cookiesProvider);
+app.use('/saved', savedRoute);
+app.use('/tickets', ticketRoute);
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 
-app.use(cors(CorsOptions)) //add cors options
-app.use(bodyParser.json());
-app.use(express.json()); //using express body-parser,  cors to ensure backend safety from another source attacks
-
-
-
-app.get('/', (req,res) => {
-    res.send('Welcome To Driver Tickets Server')
-})
-
-app.use('/data' , dataRoute);
-app.use('/cookies' , cookiesProvider);
-app.use('/saved' , savedRoute);
-app.use('/tickets', ticketRoute); //defining routes 
-
+// Start server
 const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
 });
