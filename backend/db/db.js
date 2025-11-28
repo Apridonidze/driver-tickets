48b664e9c38e24cd301769b41e9b3e50ee99d78f
unknown <giorgiapridonidze08@gmail.com>
2025-11-28 15:45:24 +0400

    added port to db middleware

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,6 +4,7 @@ require('dotenv').config(); //importing dotenv
 
 const db = mysql.createPool({ //creating pool connection to db
     host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DB,
