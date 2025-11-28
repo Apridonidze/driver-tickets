5ee5b598f4adf86b60f37cefd5d8f8990749c775
unknown <giorgiapridonidze08@gmail.com>
2025-11-28 22:24:26 +0400

    doc : documentated code and cleaned up files , (deployment ready)

--- backend/config/CorsOptions.js
+++ backend/config/CorsOptions.js
@@ -1,17 +1,15 @@
-
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173' , process.env.ORIGIN_URL];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173' , process.env.ORIGIN_URL]; //array of possible origin (frontend) urls
 
 
 const CorsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      credentials: true
-}
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }},//if origin url exists then server returns true to console, else it throws Not Allowed by Cors error
+  credentials: true //allowing credentials
+};
 
 
-module.exports = CorsOptions
+module.exports = CorsOptions;//exporting route
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express');//importing express
+const app = express();//creating server
 
-const path = require('path')
+const path = require('path'); //importing path from express
+const cors = require('cors');//importing cors
+require('dotenv').config();//importing .env
+
+const CorsOptions = require('./config/CorsOptions');//importing cors options object
+
+app.use(cors(CorsOptions));//using cors with options
+app.use(express.json());//importing express middelware for payloads
 
 const cookiesProvider = require('./routes/cookies');
 const savedRoute = require('./routes/saved');
 const ticketRoute = require('./routes/tickets');
-const dataRoute = require('./routes/data');
+const dataRoute = require('./routes/data');//importing route
 
-
-const CorsOptions = require('./config/CorsOptions');
-
-
-const PORT = process.env.PORT || 8080;
-
-const app = express();
-
-
-app.use(cors(CorsOptions));
-app.use(express.json());
+const PORT = process.env.PORT || 8080; //defining port from .env or 8080 by default
 
 app.get('/', (req,res) => {
   res.send(`Welcome To Drivers-Tickets Server , Listening on Port : ${PORT} , URL : ${process.env.BACKEND_URL}, ORIGIN URL : ${process.env.ORIGIN_URL}`)
-})
+}); //returning message to / path in backend to check that backend runs correctly
 
 app.use('/data', dataRoute);
 app.use('/cookies', cookiesProvider);
 app.use('/saved', savedRoute);
-app.use('/answered', ticketRoute);
+app.use('/answered', ticketRoute);//using routes
 
 
 app.use('/audio', express.static(path.join(__dirname, 'data/audio')));
-app.use('/ticket', express.static(path.join(__dirname, 'data/tickets')));
+app.use('/ticket', express.static(path.join(__dirname, 'data/tickets')));//using folders as routes
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
-});
+});//listening to server to check if backend runs correctly
