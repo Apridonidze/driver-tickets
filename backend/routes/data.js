bf04faf3e7990ba0b7add9da82b574734de66962
unknown <giorgiapridonidze08@gmail.com>
2025-12-19 21:38:16 +0400

    doc + fix : wrote documentation of dataRoute.js in backend/routes + fixed chunks sending to frontend

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -5,26 +5,25 @@ const tickets = require('../data/tikets.json');//importing ticket json file
 
 
 dataRoute.get('/', async (req, res) => {
-
-    try {
-    const offset = parseInt(req.query.offset) || 0;
-    const limit = 100;
+  try {
+    const offset = parseInt(req.query.offset) || 0;//number from where data should be fetched fromn
+    const limit = 100; //limit of data in one response
 
     const mappedTickets = tickets.map(ticket => ({
       ...ticket,
       QuestionAudio: `${process.env.BACKEND_URL}/audio/${ticket.QuestionAudio}`,
       DescriptionAudio: `${process.env.BACKEND_URL}/audio/${ticket.DescriptionAudio}`,
       Image: `${process.env.BACKEND_URL}/ticket/${ticket.Image}`
-    }));
+    })); //generates all data from ticket.json file and assigns audio files to it for frontend
 
-    const chunk = mappedTickets.slice(offset, offset + limit);
+    const chunk = mappedTickets.slice(offset, offset + limit); //defines sliced data from offset to offset + 100 , for frontned
 
-    return res.status(200).json({data: chunk});
+    return res.status(200).json({data: chunk});//sends data to frontend
 
   } catch (err) {
-    return res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: 'Internal Server Error' }); //returns internal error message to frontend
   }
-}); //requires
+}); //gets offset as a query from frontend , generates aall tickets on first run and assigns audio files to data, then data is sent by chucks for better performance
 
 
 module.exports = dataRoute;///exporting route
